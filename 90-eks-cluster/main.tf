cccfdfda04db6ebc5f538157e7e1a315d92d68e2
kanaka vignesh <201001039@rajalakshmi.edu.in>
2026-02-26 12:12:08 +0530

    new bucket addede

--- 90-eks-cluster/main.tf
+++ 90-eks-cluster/main.tf
@@ -1,84 +1,58 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.0"
+  version = "~> 21.0" # this is module version
 
-  # --------------------------------------------------
-  # Cluster basics
-  # --------------------------------------------------
   name               = local.common_name_suffix
+  # kubernetes_version = "1.33"
   kubernetes_version = var.eks_version
 
-  # --------------------------------------------------
-  # Control plane logging (v21 syntax)
-  # --------------------------------------------------
-  cluster_log_types = [
-    "api",
-    "audit",
-    "authenticator",
-    "controllerManager",
-    "scheduler"
-  ]
-
-  # IMPORTANT: prevent CloudWatch log group conflict
-  create_cloudwatch_log_group = false
-
-  enable_cluster_creator_admin_permissions = true
-  endpoint_public_access                    = false
-
-  # --------------------------------------------------
-  # Networking
-  # --------------------------------------------------
-  vpc_id                   = local.vpc_id
-  subnet_ids               = local.private_subnet_ids
-  control_plane_subnet_ids = local.private_subnet_ids
-
-  # --------------------------------------------------
-  # Security Groups (reuse existing)
-  # --------------------------------------------------
-  create_security_group      = false
-  create_node_security_group = false
-
-  security_group_id      = local.eks_control_plane_sg_id
-  node_security_group_id = local.eks_node_sg_id
-
-  # --------------------------------------------------
-  # EKS Addons
-  # --------------------------------------------------
   addons = {
-    coredns = {}
-
-    kube-proxy = {}
-
-    vpc-cni = {
-      before_compute = true
-    }
-
+    coredns                = {}
     eks-pod-identity-agent = {
       before_compute = true
     }
-
+    kube-proxy             = {}
+    vpc-cni                = {
+      before_compute = true
+    }
     metrics-server = {}
   }
 
-  # --------------------------------------------------
-  # Managed Node Groups (Blue / Green)
-  # --------------------------------------------------
+  endpoint_public_access = false
+  enable_cluster_creator_admin_permissions = true
+
+  vpc_id                   = local.vpc_id
+  subnet_ids               = local.private_subnet_ids
+  control_plane_subnet_ids = local.private_subnet_ids
+  create_node_security_group = false
+  create_security_group      = false
+  node_security_group_id = local.eks_node_sg_id
+  security_group_id = local.eks_control_plane_sg_id
+
+  # EKS Managed Node Group(s)
   eks_managed_node_groups = {
-
     blue = {
-      create             = var.enable_blue
-      ami_type           = "AL2023_x86_64_STANDARD"
+      create = var.enable_blue
+      ami_type       = "AL2023_x86_64_STANDARD"
       kubernetes_version = var.eks_nodegroup_blue_version
-      instance_types     = ["m5.xlarge"]
-
+      instance_types = ["m5.xlarge"]
+      iam_role_additional_policies  = {
+        amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
+        amazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+      }
+      
+      # cluster nodes autoscaling
       min_size     = 2
       max_size     = 10
       desired_size = 2
 
-      iam_role_additional_policies = {
-        amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
-        amazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-      }
+      # taints = {
+      #   upgrade = {
+      #     key = "upgrade"
+      #     value = "true"
+      #     effect = "NO_SCHEDULE"
+      #   }
+      # }
 
       labels = {
         nodegroup = "blue"
@@ -86,19 +60,27 @@ module "eks" {
     }
 
     green = {
-      create             = var.enable_green
-      ami_type           = "AL2023_x86_64_STANDARD"
+      create = var.enable_green
+      ami_type       = "AL2023_x86_64_STANDARD"
       kubernetes_version = var.eks_nodegroup_green_version
-      instance_types     = ["m5.xlarge"]
-
+      instance_types = ["m5.xlarge"]
+      iam_role_additional_policies  = {
+        amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
+        amazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+      }
+      
+      # cluster nodes autoscaling
       min_size     = 2
       max_size     = 10
       desired_size = 2
 
-      iam_role_additional_policies = {
-        amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
-        amazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-      }
+      # taints = {
+      #   upgrade = {
+      #     key = "upgrade"
+      #     value = "true"
+      #     effect = "NO_SCHEDULE"
+      #   }
+      # }
 
       labels = {
         nodegroup = "green"
@@ -106,13 +88,11 @@ module "eks" {
     }
   }
 
-  # --------------------------------------------------
-  # Tags
-  # --------------------------------------------------
   tags = merge(
     local.common_tags,
     {
-      Name = local.common_name_suffix
+        Name = local.common_name_suffix
     }
   )
+  
 }
